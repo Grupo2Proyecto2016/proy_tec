6ebb46459979c8c7798ff6e12f3a3c8b1b122633
rolo <rodrigo.navarro.uy@gmail.com>
2016-05-07 15:54:41 -0300

    Limpieza
    Eliminado

--- DataLayer/src/main/java/com/dataAccess/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dataAccess;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpresaRepository extends CrudRepository<Empresa, Long>{
-	List<Empresa> findByNombre(String nombre);
-}
