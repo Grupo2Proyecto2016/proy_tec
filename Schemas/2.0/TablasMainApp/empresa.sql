5f1242f496c12b62a562ed866efec0b103136523
rolo <rodrigo.navarro.uy@gmail.com>
2016-05-18 23:48:42 -0300

    Schemas 2.0 MainApp

---
+++ Schemas/2.0/TablasMainApp/empresa.sql
@@ -0,0 +1,24 @@
+﻿-- Table: empresa
+
+-- DROP TABLE empresa;
+
+CREATE TABLE empresa
+(
+  id_empresa serial NOT NULL,
+  nombre text NOT NULL,
+  razon_social text NOT NULL,
+  rut text NOT NULL,
+  telefono text,
+  direccion text,
+  nombre_tenant text NOT NULL,
+  id_pais bigint NOT NULL,
+  CONSTRAINT empresa_pkey PRIMARY KEY (id_empresa),
+  CONSTRAINT "empresa_pais_FK" FOREIGN KEY (id_pais)
+      REFERENCES pais (id_pais) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE empresa
+  OWNER TO postgres;
---
+++ Schemas/2.0/TablasMainApp/usuario.sql
@@ -0,0 +1,22 @@
+﻿-- Table: usuario
+
+-- DROP TABLE usuario;
+
+CREATE TABLE usuario
+(
+  id_usuario serial NOT NULL,
+  usrname text NOT NULL,
+  passwd text NOT NULL,
+  nombre text NOT NULL,
+  apellido text NOT NULL,
+  email text NOT NULL,
+  puede_crear boolean,
+  ultimo_reset_password date,
+  enabled boolean NOT NULL,
+  CONSTRAINT usuario_pkey PRIMARY KEY (id_usuario)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE usuario
+  OWNER TO postgres;
