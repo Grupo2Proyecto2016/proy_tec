104dc6b9ad2463ce4002769e998cfa882e50234f
rolo <rodrigo.navarro.uy@gmail.com>
2016-05-20 20:58:06 -0300

    Juego de datos 2.0
    Roles

---
+++ Schemas/2.0/Juego de Datos/Juego_de_datos.sql
@@ -0,0 +1,5 @@
+/*  ROLES */
+insert into rol(descripcion, nombre) values ('ADMIN', 'Administrador del sitio.');
+insert into rol(descripcion, nombre) values ('SALE', 'Empleado de Mostrador, pasajes y encomiendas.');
+insert into rol(descripcion, nombre) values ('DRIVER', 'Conductor del vehiculo.');
+insert into rol(descripcion, nombre) values ('CLIENT', 'Usuario registrado en el sitio.');
