8175dbd1b3975d96a50fa6d07defb7f9052ab050
aale21 <aale21@gmail.com>
2016-06-20 22:32:28 -0300

    Agrego el mapa al taller

--- GoOnSite/src/main/webapp/static/js/controllers/tallerController.js
+++ GoOnSite/src/main/webapp/static/js/controllers/tallerController.js
@@ -32,8 +32,10 @@ goOnApp.controller('tallerController', function($scope, $http, uiGridConstants, 
     
     $scope.showForm = function()
     {
+    	$scope.tallerForm = {};
     	$("#divTallerForm").removeClass('hidden');
-    	$scope.hideSuccess();    	
+    	$scope.hideSuccess();
+    	google.maps.event.trigger(map, 'resize');//refresh map
     };
     
     $scope.hideForm = function()
@@ -50,7 +52,14 @@ goOnApp.controller('tallerController', function($scope, $http, uiGridConstants, 
 	{		
 		if(!$scope.form.$invalid)
 		{
-			$.blockUI();			
+			$.blockUI();
+			if (($scope.tallerForm.latitud == null) || ($scope.tallerForm.longitud == null))
+			{
+				$.unblockUI();
+				$scope.error_message = 'Debe serleccionar un punto en el mapa.'; 
+				$("#errorModal").modal("toggle");
+				return;
+			}
 			$http.post(servicesUrl +'createTaller', JSON.stringify($scope.tallerForm))
 			.success(function()
 			{
@@ -113,21 +122,138 @@ goOnApp.controller('tallerController', function($scope, $http, uiGridConstants, 
 		enableFiltering: true,
         columnDefs:
     	[
+          { name:'Nombre', field: 'nombre' },
           { name:'Dirección', field: 'direccion' },
-          { name:'Teléfono', field: 'telefono' },
-          { name:'Ciudad', field: 'ciudad'},
-          { name:'Departamento', field: 'departamento' },
-          { name: 'Nombre', field: 'nombre' },
+          { name:'Teléfono', field: 'telefono'},
           
-          { name: 'Acciones',
-        	enableFiltering: false,
-        	enableSorting: false,
-            cellTemplate:'<button style="width: 50%" class="btn-xs btn-primary" ng-click="grid.appScope.getTallerDetails(row)">Detalles</button>'+
-            			 '<button style="width: 50%" class="btn-xs btn-danger" ng-click="grid.appScope.showDeleteDialog(row)">Eliminar</button>' 
-            			  
-    	  }
+//          { name: 'Acciones',
+//        	enableFiltering: false,
+//        	enableSorting: false,
+//            cellTemplate:'<button style="width: 50%" class="btn-xs btn-primary" ng-click="grid.appScope.getTallerDetails(row)">Detalles</button>'+
+//            			 '<button style="width: 50%" class="btn-xs btn-danger" ng-click="grid.appScope.showDeleteDialog(row)">Eliminar</button>' 
+//            			  
+//    	  }
         ]
      };
+	
+	//MAPA\\
+    
+    $scope.map = new google.maps.Map(document.getElementById('map'), 
+    {
+        center: {lat: -34.894418, lng: -56.165775},
+        zoom: 13
+    });
+    			 
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    
+ // Bias the SearchBox results towards current map's viewport.
+    $scope.map.addListener('bounds_changed', function() 
+    {
+      searchBox.setBounds($scope.map.getBounds());
+    });
+    
+    $scope.markers = [];
+    
+    searchBox.addListener('places_changed', function() 
+    {
+    	var places = searchBox.getPlaces();
+
+        if (places.length == 0) 
+        {
+          return;
+        }
+        
+        //Clear out the old markers.
+        $scope.markers.forEach(function(marker) 
+        {
+          marker.setMap(null);
+        });
+        $scope.markers = [];
+        
+     // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) 
+        {
+         
+       // Create a marker for each place.
+          $scope.markers.push(new google.maps.Marker({
+            map: $scope.map,
+            title: place.name,
+            position: place.geometry.location
+          }));
+
+          if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+          } else {
+            bounds.extend(place.geometry.location);
+          }
+        });                 
+        $scope.map.fitBounds(bounds);
+        $scope.tallerForm.direccion = places[0].formatted_address;
+        $scope.actualizoMarker(places[0].geometry.location.lat(), places[0].geometry.location.lng());
+        $scope.$digest();
+    });
+	     
+    
+    $scope.map.addListener('click', function(e) 
+    {
+    	//Clear out the old markers.
+        $scope.markers.forEach(function(marker) 
+        {
+          marker.setMap(null);
+        });
+        $scope.markers = [];
+        
+    	$scope.placeMarkerAndPanTo(e.latLng, $scope.map);
+	});
+    
+    $scope.placeMarkerAndPanTo = function (latLng, map) 
+    {
+  	  var marker = new google.maps.Marker({
+  	    position: latLng,
+  	    map: $scope.map
+  	  });
+  	  $scope.markers.push(marker);
+  	  var l = latLng.lat();
+  	  var g = latLng.lng();
+  	  $scope.actualizoMarker(l, g);
+  	  $scope.map.panTo(latLng);
+  	  //
+  	  var elevationService = new google.maps.ElevationService();
+  	  var requestElevation = 
+  	  {
+  		'locations': [marker.getPosition()]
+  	  };  	  
+   	 elevationService.getElevationForLocations(requestElevation, function(results, status) 
+   	 {
+  	    if (status == google.maps.ElevationStatus.OK) {
+  	      if (results[0]) 
+  	      {
+  	        if (parseFloat(results[0].elevation) < 1)
+  	        {
+  	        	//Es Agua
+  	        	marker.setMap(null);
+  	        	$scope.markers = [];
+  	        	$scope.error_message = 'Punto del mapa incorrecto, esta en el agua!';
+  	        	$scope.tallerForm.latitud = null;
+  	        	$scope.tallerForm.longitud = null;
+  	        	$scope.$digest();
+				$("#errorModal").modal("toggle");
+  	        }  	    	  
+  	      }
+  	    }
+  	  });
+   	}
+    
+    $scope.actualizoMarker = function (lat, lng)
+    {
+    	$scope.tallerForm.latitud = lat;    	                  
+    	$scope.tallerForm.longitud = lng;
+    };
 	
 	$scope.showDeleteDialog = function(row)
     {
